85563ff791302121fbed49a5783ee0c62bda230f
Sergey Velichko <s.v.velichko.06@gmail.com>
2026-09-10 20:05:34 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,136 @@
+from contextlib import asynccontextmanager
+from uuid import uuid4
+
+from fastapi import FastAPI, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+
+DATABASE_URL = "postgresql+psycopg://postgres:admin@127.0.0.1:15432/postgres"
+engine = create_engine(DATABASE_URL)
+Sessionlocal = sessionmaker(bind=engine)
+
+
+class Base(DeclarativeBase):
+    id: Mapped[str] = mapped_column(primary_key=True, default=lambda: str(uuid4()))
+
+
+class TaskORM(Base):
+    __tablename__ = "tasks"
+
+    title: Mapped[str]
+    completed: Mapped[bool] = mapped_column(default=False)
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+)
+
+
+class TaskSchema(BaseModel):
+    id: str
+    title: str
+    completed: bool
+
+
+class TaskCreateSchema(BaseModel):
+    title: str
+
+
+class TaskUpdateSchema(BaseModel):
+    title: str | None = None
+    completed: bool | None = None
+
+
+class CategorySchema(BaseModel):
+    id: str
+    name: str
+
+
+class CategoryCreateSchema(BaseModel):
+    name: str
+
+
+class CategoryUpdateSchema(BaseModel):
+    name: str | None = None
+
+
+tasks: list[TaskSchema] = []
+categories: list[CategorySchema] = []
+
+
+@app.get("/tasks")
+def read_tasks() -> list[TaskSchema]:
+    return tasks
+
+
+@app.post("/tasks", status_code=status.HTTP_201_CREATED)
+
+def create_task(payload: TaskCreateSchema) -> TaskSchema:
+    new_task = TaskSchema(id=str(uuid4()), title=payload.title, completed=False)
+    tasks.append(new_task)
+    return new_task
+
+
+@app.patch("/tasks/{task_id}")
+def update_task(task_id: str, payload: TaskUpdateSchema):
+    for task in tasks:
+        if task.id == task_id:
+            if payload.title:
+                task.title = payload.title
+            if payload.completed is not None:
+                task.completed = payload.completed
+            return task
+
+
+@app.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(task_id: str):
+    for task in tasks:
+        if task.id == task_id:
+            tasks.remove(task)
+
+
+@app.get("/categories")
+def read_categories() -> list[CategorySchema]:
+    return categories
+
+
+@app.post("/categories", status_code=status.HTTP_201_CREATED)
+def create_category(payload: CategoryCreateSchema) -> CategorySchema:
+    new_category = CategorySchema(id=str(uuid4()), name=payload.name)
+    categories.append(new_category)
+    return new_category
+
+
+@app.patch("/categories/{category_id}")
+def update_category(category_id: str, payload: CategoryUpdateSchema):
+    for category in categories:
+        if category.id == category_id:
+            if payload.name:
+                category.name = payload.name
+            return category
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Category not found"
+    )
+
+
+@app.delete("/categories/{category_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_category(category_id: str):
+    for category in categories:
+        if category.id == category_id:
+            categories.remove(category)
+            return
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Category not found"
+    )
