ee82e5789a032e41a326068e0d111a75ab098b68
Sergey Velichko <s.v.velichko.06@gmail.com>
2026-09-24 18:15:06 +0500

    annotated get_db function

--- app/db/session.py
+++ app/db/session.py
@@ -1,13 +1,16 @@
+from collections.abc import Iterator
+
 from app.core.config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
 
 engine = create_engine(settings.database_url)
-Sessionlocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(bind=engine)
 
 
-def get_db():
-    db = Sessionlocal()
+def get_db() -> Iterator[Session]:
+    db = SessionLocal()
     try:
         yield db
     finally:
