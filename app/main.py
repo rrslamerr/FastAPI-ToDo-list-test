97a0de855dfa50a2325e6e1db921f7331bed0c6f
Sergey Velichko <s.v.velichko.06@gmail.com>
2026-09-24 18:14:49 +0500

    add alembic

--- app/main.py
+++ app/main.py
@@ -1,21 +1,10 @@
-from contextlib import asynccontextmanager
-
 from app.api.routers.category import router as category_router
 from app.api.routers.task import router as task_router
 from app.core.config import settings
-from app.db.session import engine
-from app.models.base import Base
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-
-@asynccontextmanager
-async def lifespan(_: FastAPI):
-    Base.metadata.create_all(bind=engine)
-    yield
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 app.include_router(task_router)
 app.include_router(category_router)
