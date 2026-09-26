64311b442bbca1ec0fa4317d2c84016f4e0a6187
Sergey Velichko <s.v.velichko.06@gmail.com>
2026-09-26 09:00:39 +0500

    add async get db session

--- app/db/session.py
+++ app/db/session.py
@@ -1,17 +1,13 @@
-from collections.abc import Iterator
+from collections.abc import AsyncGenerator
 
 from app.core.config import settings
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio.session import AsyncSession
 
-engine = create_engine(settings.database_url)
-SessionLocal = sessionmaker(bind=engine)
+engine = create_async_engine(settings.database_url)
+SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 
-def get_db() -> Iterator[Session]:
-    db = SessionLocal()
-    try:
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with SessionLocal() as db:
         yield db
-    finally:
-        db.close()
