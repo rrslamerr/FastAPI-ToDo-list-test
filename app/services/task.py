ed150a21f59988d386ae343278cca475d44902d7
Sergey Velichko <s.v.velichko.06@gmail.com>
2026-09-26 09:19:12 +0500

    convert to aasync task service

--- app/services/task.py
+++ app/services/task.py
@@ -3,37 +3,39 @@ from uuid import UUID
 from app.core.exceptions import TaskNotFound
 from app.repositories.task import TaskRepository
 from app.schemas.task import TaskCreateSchema, TaskSchema, TaskUpdateSchema
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class TaskService:
-    def __init__(self, db: Session) -> None:
+    def __init__(self, db: AsyncSession) -> None:
         self.db = db
         self.task_repository = TaskRepository(db=db)
 
-    def list_tasks(self) -> list[TaskSchema]:
-        tasks_orm = self.task_repository.get_all()
+    async def list_tasks(self) -> list[TaskSchema]:
+        tasks_orm = await self.task_repository.get_all()
         return [TaskSchema.model_validate(task) for task in tasks_orm]
 
-    def create_task(self, task_create: TaskCreateSchema) -> TaskSchema:
+    async def create_task(self, task_create: TaskCreateSchema) -> TaskSchema:
         task_orm = self.task_repository.create(title=task_create.title)
-        self.db.commit()
+        await self.db.commit()
         return TaskSchema.model_validate(task_orm)
 
-    def update_task(self, task_id: UUID, task_update: TaskUpdateSchema) -> TaskSchema:
-        task_for_update = self.task_repository.get_by_id(task_id=task_id)
+    async def update_task(
+        self, task_id: UUID, task_update: TaskUpdateSchema
+    ) -> TaskSchema:
+        task_for_update = await self.task_repository.get_by_id(task_id=task_id)
         if task_for_update is None:
             raise TaskNotFound(f"Task with id {task_id} not found")
         if task_update.title is not None:
             task_for_update.title = task_update.title
         if task_update.completed is not None:
             task_for_update.completed = task_update.completed
-        self.db.commit()
+        await self.db.commit()
         return TaskSchema.model_validate(task_for_update)
 
-    def delete_task(self, task_id: UUID) -> None:
-        task_for_delete = self.task_repository.get_by_id(task_id=task_id)
+    async def delete_task(self, task_id: UUID) -> None:
+        task_for_delete = await self.task_repository.get_by_id(task_id=task_id)
         if task_for_delete is None:
             raise TaskNotFound(f"Task with id {task_id} not found")
-        self.task_repository.delete(task_for_delete)
-        self.db.commit()
+        await self.task_repository.delete(task_for_delete)
+        await self.db.commit()
