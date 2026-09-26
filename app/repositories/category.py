5d84fc196974b670af5eef908b2ea1d7539b873e
Sergey Velichko <s.v.velichko.06@gmail.com>
2026-09-26 09:10:41 +0500

    convert to async category repository

--- app/repositories/category.py
+++ app/repositories/category.py
@@ -2,24 +2,24 @@ from uuid import UUID
 
 from app.models.category import CategoryORM
 from sqlalchemy import select
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class CategoryRepository:
-    def __init__(self, db: Session) -> None:
+    def __init__(self, db: AsyncSession) -> None:
         self.db = db
 
-    def get_all(self) -> list[CategoryORM]:
-        return list(self.db.scalars(select(CategoryORM)).all())
+    async def get_all(self) -> list[CategoryORM]:
+        return list((await self.db.scalars(select(CategoryORM))).all())
 
-    def get_by_id(self, category_id: UUID) -> CategoryORM | None:
-        category = self.db.get(CategoryORM, category_id)
+    async def get_by_id(self, category_id: UUID) -> CategoryORM | None:
+        category = await self.db.get(CategoryORM, category_id)
         return category
 
-    def create(self, name: str) -> CategoryORM:
+    async def create(self, name: str) -> CategoryORM:
         new_category = CategoryORM(name=name)
         self.db.add(new_category)
         return new_category
 
-    def delete(self, category: CategoryORM) -> None:
-        self.db.delete(category)
+    async def delete(self, category: CategoryORM) -> None:
+        await self.db.delete(category)
