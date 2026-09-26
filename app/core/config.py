e515dc98c9d4e659b355d6c0b9c9b5516c2cd4a2
Sergey Velichko <s.v.velichko.06@gmail.com>
2026-09-26 08:54:56 +0500

    add asyncpg in database_url

--- app/core/config.py
+++ app/core/config.py
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
 
     @property
     def database_url(self) -> str:
-        return f"postgresql+psycopg://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
+        return f"postgresql+asyncpg://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
 
 
 settings = Settings()  # type: ignore
