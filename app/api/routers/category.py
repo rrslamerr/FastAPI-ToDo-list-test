f64d06a0e474a7545e16913adc50ba4df3202567
Sergey Velichko <s.v.velichko.06@gmail.com>
2026-09-26 09:22:26 +0500

    convert to async category api

--- app/api/routers/category.py
+++ app/api/routers/category.py
@@ -13,27 +13,27 @@ router = APIRouter(prefix="/categories")
 
 
 @router.get("")
-def read_categories(
+async def read_categories(
     category_service: CategoryServiceDep,
 ) -> list[CategorySchema]:
-    return category_service.list_categories()
+    return await category_service.list_categories()
 
 
 @router.post("", status_code=status.HTTP_201_CREATED)
-def create_category(
+async def create_category(
     payload: CategoryCreateSchema, category_service: CategoryServiceDep
 ) -> CategorySchema:
-    return category_service.create_category(category_create=payload)
+    return await category_service.create_category(category_create=payload)
 
 
 @router.patch("/{category_id}")
-def update_category(
+async def update_category(
     category_id: UUID,
     payload: CategoryUpdateSchema,
     category_service: CategoryServiceDep,
 ) -> CategorySchema:
     try:
-        return category_service.update_category(
+        return await category_service.update_category(
             category_id=category_id, category_update=payload
         )
     except CategoryNotFound as e:
@@ -41,8 +41,10 @@ def update_category(
 
 
 @router.delete("/{category_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_category(category_id: UUID, category_service: CategoryServiceDep) -> None:
+async def delete_category(
+    category_id: UUID, category_service: CategoryServiceDep
+) -> None:
     try:
-        category_service.delete_category(category_id=category_id)
+        await category_service.delete_category(category_id=category_id)
     except CategoryNotFound as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
