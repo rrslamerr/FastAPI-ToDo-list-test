66cde40e9b776547c67023b021b36a869be29ea8
Sergey Velichko <s.v.velichko.06@gmail.com>
2026-09-26 09:16:29 +0500

    convert to async category service

--- app/services/category.py
+++ app/services/category.py
@@ -7,41 +7,43 @@ from app.schemas.category import (
     CategorySchema,
     CategoryUpdateSchema,
 )
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class CategoryService:
-    def __init__(self, db: Session) -> None:
+    def __init__(self, db: AsyncSession) -> None:
         self.db = db
         self.category_repository = CategoryRepository(db=db)
 
-    def list_categories(self) -> list[CategorySchema]:
-        categories_orm = self.category_repository.get_all()
+    async def list_categories(self) -> list[CategorySchema]:
+        categories_orm = await self.category_repository.get_all()
         return [CategorySchema.model_validate(category) for category in categories_orm]
 
-    def create_category(self, category_create: CategoryCreateSchema) -> CategorySchema:
-        category_orm = self.category_repository.create(name=category_create.name)
-        self.db.commit()
+    async def create_category(
+        self, category_create: CategoryCreateSchema
+    ) -> CategorySchema:
+        category_orm = await self.category_repository.create(name=category_create.name)
+        await self.db.commit()
         return CategorySchema.model_validate(category_orm)
 
-    def update_category(
+    async def update_category(
         self, category_id: UUID, category_update: CategoryUpdateSchema
     ) -> CategorySchema:
-        category_for_update = self.category_repository.get_by_id(
+        category_for_update = await self.category_repository.get_by_id(
             category_id=category_id
         )
         if category_for_update is None:
             raise CategoryNotFound(f"Category with id {category_id} not found")
         if category_update.name is not None:
             category_for_update.name = category_update.name
-        self.db.commit()
+        await self.db.commit()
         return CategorySchema.model_validate(category_for_update)
 
-    def delete_category(self, category_id: UUID) -> None:
-        category_for_delete = self.category_repository.get_by_id(
+    async def delete_category(self, category_id: UUID) -> None:
+        category_for_delete = await self.category_repository.get_by_id(
             category_id=category_id
         )
         if category_for_delete is None:
             raise CategoryNotFound(f"Category with id {category_id} not found")
-        self.category_repository.delete(category_for_delete)
-        self.db.commit()
+        await self.category_repository.delete(category_for_delete)
+        await self.db.commit()
