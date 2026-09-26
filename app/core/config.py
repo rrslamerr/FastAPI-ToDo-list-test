d6f4d40697f1e6034526e7991456ad92c4605a44
Sergey Velichko <s.v.velichko.06@gmail.com>
2026-09-26 22:04:51 +0500

    add ignore pyright

--- app/core/config.py
+++ app/core/config.py
@@ -16,4 +16,4 @@ class Settings(BaseSettings):
         return f"postgresql+asyncpg://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
 
 
-settings = Settings()  # type: ignore
+settings = Settings()  # pyright: ignore[reportCallIssue]
