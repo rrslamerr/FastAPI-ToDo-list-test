0965c1cca849bff41cbd8966eb43a4d182d9bb74
Sergey Velichko <s.v.velichko.06@gmail.com>
2026-09-26 09:22:37 +0500

    convert to async task api

--- app/api/routers/task.py
+++ app/api/routers/task.py
@@ -13,36 +13,36 @@ router = APIRouter(prefix="/tasks")
 
 
 @router.get("")
-def read_tasks(task_service: TaskServiceDep) -> list[TaskSchema]:
-    return task_service.list_tasks()
+async def read_tasks(task_service: TaskServiceDep) -> list[TaskSchema]:
+    return await task_service.list_tasks()
 
 
 @router.post("", status_code=status.HTTP_201_CREATED)
-def create_task(
+async def create_task(
     payload: TaskCreateSchema,
     task_service: TaskServiceDep,
 ) -> TaskSchema:
-    return task_service.create_task(task_create=payload)
+    return await task_service.create_task(task_create=payload)
 
 
 @router.patch("/{task_id}")
-def update_task(
+async def update_task(
     task_id: UUID,
     payload: TaskUpdateSchema,
     task_service: TaskServiceDep,
 ) -> TaskSchema:
     try:
-        return task_service.update_task(task_id=task_id, task_update=payload)
+        return await task_service.update_task(task_id=task_id, task_update=payload)
     except TaskNotFound as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
 
 
 @router.delete("/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_task(
+async def delete_task(
     task_id: UUID,
     task_service: TaskServiceDep,
 ) -> None:
     try:
-        return task_service.delete_task(task_id=task_id)
+        return await task_service.delete_task(task_id=task_id)
     except TaskNotFound as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
